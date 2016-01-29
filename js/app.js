81546cee0e4171633558834443a09090cb51a2b7
Andrew Portell <dportell@gmail.com>
2016-01-28 19:15:10 -0600

    removed trailing whitespace and beautified before submitting project

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ var Enemy = function() {
     this.y = 63 + (Math.round(Math.random() * 2) * 83);
     this.speed = (Math.random() * 125) + 50;
     this.sprite = 'images/enemy-bug.png';
-};
+}
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -16,16 +16,16 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     this.x += this.speed * dt;
 
-   if(this.x >= 505) {
+    if (this.x >= 505) {
         this.x = -101;
         this.y = 63 + (Math.round(Math.random() * 2) * 83);
-    }           
-};
+    }
+}
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -33,14 +33,14 @@ Enemy.prototype.render = function() {
 var Player = function() {
     this.reset();
     this.score = 0;
-    this.sprite = "images/char-boy.png";
-};
+    this.sprite = 'images/char-boy.png';
+}
 
 Player.prototype.update = function() {
     for (var i in allEnemies) {
-        if (this.x < allEnemies[i].x + 90 && 
-            this.x + 65 > allEnemies[i].x + 2 && 
-            this.y + 135 > allEnemies[i].y + 142 && 
+        if (this.x < allEnemies[i].x + 90 &&
+            this.x + 65 > allEnemies[i].x + 2 &&
+            this.y + 135 > allEnemies[i].y + 142 &&
             this.y + 65 < allEnemies[i].y + 79) {
             this.score -= 1;
             this.reset();
@@ -56,29 +56,29 @@ Player.prototype.update = function() {
     ctx.font = '20px Comic Sans MS';
     ctx.fillStyle = 'blue';
     ctx.clearRect(0, 0, 450, 150);
-    ctx.fillText("My Score: " + this.score, 195, 35);
-};
+    ctx.fillText('My Score: ' + this.score, 195, 35);
+}
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+}
 
 Player.prototype.handleInput = function(key) {
-    if(key == 'left' && this.x - 101 >= 0)
+    if (key == 'left' && this.x - 101 >= 0)
         this.x -= 101;
-    if(key == 'up' && this.y - 83 >= -11)
+    if (key == 'up' && this.y - 83 >= -11)
         this.y -= 83;
-    if(key == 'right' && this.x + 101 < 505)
+    if (key == 'right' && this.x + 101 < 505)
         this.x += 101;
-    if(key == 'down' && this.y + 83 < 487)
+    if (key == 'down' && this.y + 83 < 487)
         this.y += 83;
-};
+}
 
 // Reset Player to starting position
 Player.prototype.reset = function() {
     this.x = 202;
     this.y = 404;
-};
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
